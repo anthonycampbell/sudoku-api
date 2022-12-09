019772d01c671eac6832c5703c6d14c136d16ca0
Anthony Campbell <anthony-campbell@hotmail.com>
2022-12-08 20:08:57 -0800

    Modify algorithm for different python version.

--- server.py
+++ server.py
@@ -1,15 +1,12 @@
 
 from flask import Flask, request, jsonify
-import sys
-import logging
 import cv2
 from imutils import contours
 import numpy as np
 import pytesseract
 
 app = Flask(__name__)
-app.logger.addHandler(logging.StreamHandler(sys.stdout))
-app.logger.setLevel(logging.ERROR)
+
 
 @app.route('/getmsg/', methods=['GET'])
 def respond():
@@ -59,7 +56,8 @@ def index():
     # Load image, grayscale, and adaptive threshold
     image = cv2.imread('sudoku.png')
     gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-    thresh = cv2.adaptiveThreshold(gray,255,cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY_INV,57,5)
+    thresh = cv2.adaptiveThreshold(
+        gray, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY_INV, 57, 5)
 
     # Filter out all numbers and noise to isolate only boxes
     cnts = cv2.findContours(thresh, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
@@ -67,15 +65,17 @@ def index():
     for c in cnts:
         area = cv2.contourArea(c)
         if area < 1000:
-            cv2.drawContours(thresh, [c], -1, (0,0,0), -1)
+            cv2.drawContours(thresh, [c], -1, (0, 0, 0), -1)
 
     # Fix horizontal and vertical lines
-    vertical_kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (1,5))
-    thresh = cv2.morphologyEx(thresh, cv2.MORPH_CLOSE, vertical_kernel, iterations=9)
-    horizontal_kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (5,1))
-    thresh = cv2.morphologyEx(thresh, cv2.MORPH_CLOSE, horizontal_kernel, iterations=4)
+    vertical_kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (1, 5))
+    thresh = cv2.morphologyEx(thresh, cv2.MORPH_CLOSE,
+                              vertical_kernel, iterations=9)
+    horizontal_kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (5, 1))
+    thresh = cv2.morphologyEx(thresh, cv2.MORPH_CLOSE,
+                              horizontal_kernel, iterations=4)
 
-    dilation = cv2.dilate(thresh, vertical_kernel, iterations = 1)
+    dilation = cv2.dilate(thresh, vertical_kernel, iterations=1)
 
     # Sort by top to bottom and each row by left to right
     invert = 255 - thresh
@@ -99,16 +99,22 @@ def index():
     for row in sudoku_rows:
         for c in row:
             mask = np.zeros(image.shape, dtype=np.uint8)
-            cv2.drawContours(mask, [c], -1, (255,255,255), -1)
+            cv2.drawContours(mask, [c], -1, (255, 255, 255), -1)
             result = cv2.bitwise_and(image, mask)
-            result[mask==0] = 255
+            result[mask == 0] = 255
             text = pytesseract.image_to_string(result)
             x, y, w, h = cv2.boundingRect(c)
-            rect = cv2.rectangle(result, (x, y), (x + w, y + h), (0, 255, 0), 2)
+            rect = cv2.rectangle(
+                result, (x, y), (x + w, y + h), (0, 255, 0), 2)
             cropped = result[y:y + h, x:x + w]
-            text = pytesseract.image_to_string(cropped,lang="eng", config='--psm 9 --oem 3 -c tessedit_char_whitelist=0123456789')
+            text = pytesseract.image_to_string(
+                cropped, lang="eng", config='--psm 9 --oem 3 -c tessedit_char_whitelist=0123456789')
             text = text.replace("\n", "")
-            sudoku_string += text or '0'
+            if (text == ""):
+                sudoku_string += '0'
+            else:
+                sudoku_string += text
+
     # A welcome message to test our server
     return sudoku_string
 
